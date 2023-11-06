64fb4d814c63d0d3517e7c0f02d7806dba46d782
AH <ahgh.com@yahoo.com>
2023-11-06 07:35:15 -0800

    Creat a registration form

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TimeField()
+    content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User , on_delete=models.CASCADE)
---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {useranme}!')
+            return redirect('blog-home')
+        else:
+            form = UserCreationForm()
+    return render(request, 'users/register.html', {'form':form})
