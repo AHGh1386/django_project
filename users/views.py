fcb481f4dc6a9fffd04989f3b48f3ad5d6eac740
Asus <Asus@DESKTOP-G3J1S9H>
2023-11-11 17:53:01 +0330

    add profile.html and make some changes in views.py for profile page

--- users/views.py
+++ users/views.py
@@ -9,8 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'your Account Has Been Created! Now You Can Log In')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+
+def profile(request):
+    return render(request, "users/profile.html")
